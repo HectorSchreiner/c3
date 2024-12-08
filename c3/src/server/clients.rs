0fc0c458b71bb90e63d45b68d5b7f20cfd81eaf6
Hector <agenthector007@gmail.com>
2024-12-08 18:05:40 +0100

    networking

--- c3/src/server/clients.rs
+++ c3/src/server/clients.rs
@@ -1,5 +1,6 @@
 use std::net::TcpStream;
 use std::time::Instant;
+use time::{OffsetDateTime, PrimitiveDateTime};
 use uuid::Uuid;
 
 #[derive(Debug)]
@@ -8,8 +9,25 @@ pub struct Client {
     pub stream: TcpStream,
     pub hostname: String,
     pub ip_address: String,
-    pub last_seen: Instant,
+    pub last_seen: PrimitiveDateTime,
     pub status: ClientStatus,
+}
+
+impl Client {
+    pub fn new(
+        id: Uuid,
+        stream: TcpStream,
+        hostname: String,
+        ip_address: String,
+    ) -> Self 
+    {
+        let now = OffsetDateTime::now_utc();
+        let date = now.date();
+        let time = now.time();
+        let last_seen = PrimitiveDateTime::new(date, time);
+        let status = ClientStatus::Connected;
+        Self { id, stream, hostname, ip_address, last_seen, status }
+    }
 }
 
 #[derive(Debug, PartialEq)]
